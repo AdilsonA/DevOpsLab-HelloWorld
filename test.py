8ad07893f137a7632ddcdc6454e8ab665b190678
adilsondandrade64 <adilsondandrade64@gmail.com>
2020-10-16 17:35:49 -0400

    Teste.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Teste DEVOPS essentials for Linux - Adilson")
+        self.assertRegex(result.data.decode(), "Teste DEVOPS essentials for Linux - Novo")
 
 
 if __name__ == "__main__":
